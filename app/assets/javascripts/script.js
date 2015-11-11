2b00ca42730ab892069170c18d4f5f4bcd14205f
Dan W <dantech88@yahoo.com>
2015-11-11 04:07:45 +0000

    added mouse functions

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,19 +3,51 @@ $(document).ready(function(){
   canvas = document.getElementById("test_canvas");
   ctx = canvas.getContext("2d");
   
-  var editor = CodeMirror(document.getElementById("input_text"), {
+  tab_containers = [$("#drawFunc").html(), $("#downFunc").html(), $("#upFunc").html(), $("#moveFunc").html(), $("#leaveFunc").html()]
+  
+  editor = CodeMirror(document.getElementById("input_text"), {
     lineNumbers: true,
-    value: $("#writeable").html(),
+    value: tab_containers[current_tab],
     mode:  "javascript",
   });
   
   $("#submit").click(function(){
-    eval(editor.getValue());
+    evalTabs();
     draw();
+  });
+  
+  $("#test_canvas").mousedown(function(){
+    mouseDown();
+  });
+  
+  $("#test_canvas").mouseup(function(){
+    mouseUp();
+  });
+  
+  $("#test_canvas").mousemove(function(){
+    mouseMove();
+  });
+  
+  $("#test_canvas").mouseleave(function(){
+    mouseLeave();
+  });
+  
+  $(".tab").click(function(){
+    $("[tab_link="+current_tab+"]").removeClass("selected");
+    evalTabs();
+    current_tab = parseInt($(this).attr("tab_link"));
+    editor.setValue(tab_containers[current_tab]);
+    $(this).addClass("selected");
   });
   
   draw();
 });
+
+function evalTabs(){
+  tab_containers[current_tab] = editor.getValue();
+  $("#drawFunc").html(tab_containers[current_tab]);
+  eval(tab_containers[current_tab]);
+}
 
 window.onerror = function(message, url, lineNumber) {  
   addMessage(message+" (line "+lineNumber+")");
@@ -37,9 +69,20 @@ function addMessage(message){
 canvas = null;
 /*global ctx*/
 ctx = null;
-/*global draw*/
-/*global CodeMirror*/
 /*global canvasWidth*/
 canvasWidth = 400;
 /*global canvasHeight*/
 canvasHeight = 400;
+/*global tab_containers*/
+tab_containers = null;
+/*global current_tab*/
+current_tab = 0;
+/*global editor*/
+editor = null
+
+/*global draw*/
+/*global mouseDown*/
+/*global mouseUp*/
+/*global mouseMove*/
+/*global mouseLeave*/
+/*global CodeMirror*/
